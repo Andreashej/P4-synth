14523bdcba14304481091a2722497c873ee6322c
Andreas Hejndorf <ahejnd15@student.aau.dk>
2017-04-05 14:35:18 +0200

    Added comments and ardiono code for ultrasonic sensor

--- UnityProject/Assets/Scripts/tcpserver.cs
+++ UnityProject/Assets/Scripts/tcpserver.cs
@@ -13,11 +13,11 @@ public class tcpserver : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        client = new TcpClient();
-        client.Connect("localhost", 17435);
-        stream = client.GetStream();
-        asEn = new System.Text.ASCIIEncoding();
-        PDSend("/dsp 1");
+        client = new TcpClient(); //Create new instance of TCP Client
+        client.Connect("localhost", 17435); //Connect TCP client to port 17435
+        stream = client.GetStream(); //get the TCP stream
+        asEn = new System.Text.ASCIIEncoding(); //Init ASCII encoded string
+        PDSend("/dsp 1"); //Turn DSP on in PD
 	}
 	
 	// Update is called once per frame
@@ -26,13 +26,13 @@ public class tcpserver : MonoBehaviour {
 	}
 
     public static void PDSend(string msg) {
-        string message = msg +";";
-        byte[] byteMsg = asEn.GetBytes(message);
-        stream.Write(byteMsg, 0, byteMsg.Length);
+        string message = msg +";"; //Store message and add ; to end of line
+        byte[] byteMsg = asEn.GetBytes(message); //Convert the message to a byte array to send
+        stream.Write(byteMsg, 0, byteMsg.Length); //Write the byte array to the stream
     }
 
     private void OnApplicationQuit () {
-        PDSend("/dsp 0");
-        client.Close();  
+        PDSend("/dsp 0"); //Turn DSP off at application quit
+        client.Close(); //Close TCP client when application quit
     }
 }
